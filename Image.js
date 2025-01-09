63618a1b967164e0ce9f4edafcd5a4c6a3e3dfc7
Rolex-arjun <143622488+Rolex-arjun@users.noreply.github.com>
2025-01-09 19:22:51 +0530

    Create Image.js

---
+++ Image.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const imageSchema = new mongoose.Schema({
+  prompt: String,
+  imageUrl: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Image', imageSchema);
